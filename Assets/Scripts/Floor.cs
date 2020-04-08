acf7c6ff77b2954fa854fe4f783902c781d19c04
Yusuke Takeuchi <v.takeuchi+gh@gmail.com>
2020-04-09 05:45:26 +0900

    feat: infinite floors

--- Assets/Scripts/Floor.cs
+++ Assets/Scripts/Floor.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Floor : MonoBehaviour
 {
+    public long FloorNumber { get; set; }
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -8,35 +8,86 @@ public class Tower : MonoBehaviour
 
     public const float PPU = Global.PPU;
 
+    public const long FALLING_SPEED_MAX = 4999;
+    public const long FALL_COUNT_LIMIT = 20000;
+
+    public const float BOTTOM_FLOOR_Y = -3.0f;
+    public const float FLOOR_INTERVAL = 80.0f / PPU;
+    // indicates the area where objects are available by
+    // designating the length from the center of the camera
+    public const float PRESENTATION_HEIGHT = 4.8f;
+    public const float CAMERA_WRAP_LIMIT = 9.6f;
+
+    private GameObject mainCamera;
+
+    private long bottomFloorNumber;
+    private long lastCreatedFloorNumber;
+
+    private long fallingSpeed = 0;
+    private long fallCount = 0;
+
+    private readonly Queue<GameObject> floors = new Queue<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
         floorPrefab = Resources.Load("Prefabs/Floor") as GameObject;
 
-        CreateRandomFloor(-1.0f);
-        CreateRandomFloor(0.0f);
-        CreateRandomFloor(1.0f);
-        CreateRandomFloor(2.0f);
-        CreateRandomFloor(3.0f);
+        mainCamera = Camera.main.gameObject;
+
+        bottomFloorNumber = 0;
+        lastCreatedFloorNumber = 0;
+
+        PrepareAvailableFloors();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-
+        if (fallingSpeed < FALLING_SPEED_MAX)
+        {
+            fallingSpeed++;
+        }
+        fallCount += fallingSpeed;
+        if (fallCount > FALL_COUNT_LIMIT)
+        {
+            fallCount -= FALL_COUNT_LIMIT;
+            Fall();            
+        }
     }
 
-    private void CreateRandomFloor(float y)
+    private void PrepareAvailableFloors()
+    {
+        float cameraY = mainCamera.transform.position.y;
+        float minY = cameraY - PRESENTATION_HEIGHT;
+        float maxY = cameraY + PRESENTATION_HEIGHT;
+
+        long floorNum = lastCreatedFloorNumber + 1;
+        float y = BOTTOM_FLOOR_Y + (floorNum - bottomFloorNumber) * FLOOR_INTERVAL;
+        while (y <= maxY)
+        {
+            if (minY <= y)
+            {
+                // TODO: implement checking big floor
+                CreateRandomFloor(floorNum, y);
+                lastCreatedFloorNumber = floorNum;
+            }
+            floorNum++;
+            y = BOTTOM_FLOOR_Y + (floorNum - bottomFloorNumber) * FLOOR_INTERVAL;
+        }
+    }
+
+    private void CreateRandomFloor(long floorNum, float y)
     {
         float width = (4 + Random.Range(0.0f, 2.0f) + Random.Range(0.0f, 2.0f)) * 32.0f / PPU;
         float x_min = Global.TOWER_LEFT_WALL_X + width / 2;
         float x_max = Global.TOWER_RIGHT_WALL_X - width / 2;
-        CreateFloor(Random.Range(x_min, x_max), y, width);
+        CreateFloor(floorNum, Random.Range(x_min, x_max), y, width);
     }
 
-    private void CreateFloor(float x, float y, float width)
+    private void CreateFloor(long floorNum, float x, float y, float width)
     {
-        GameObject floor = Instantiate(floorPrefab) as GameObject;
+        GameObject floor = Instantiate(floorPrefab) as GameObject ;
 
         var pos = floor.transform.position;
         pos.x = x;
@@ -48,5 +99,18 @@ public class Tower : MonoBehaviour
         size.x = width;
         sprite.size = size;
 
+        floor.GetComponent<Floor>().FloorNumber = floorNum;
+
+        floors.Enqueue(floor);
+
+        Debug.Log($"Floor Created: {floorNum}");
+    }
+
+    private void Fall()
+    {
+        PrepareAvailableFloors();
+        var position = mainCamera.transform.position;
+        position.y += 0.08f;
+        mainCamera.transform.position = position;
     }
 }
